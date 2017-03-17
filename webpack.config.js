86f60c2bfc67b02af3e6ba32f7a062ca8e9505c3
Ivin Jose <ivinjose@gmail.com>
2017-03-17 08:25:02 +0530

    Integrated with postcss-loader and postcss-nested

--- webpack.config.js
+++ webpack.config.js
@@ -18,20 +18,19 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: ['style-loader'],
-			},
-			{
-				test: /\.css$/,
 				use: [
+					'style-loader',
 					{
 						loader: 'css-loader',
 						options: {
 							modules: true,
 							localIdentName: '[name]__[local]___[hash:base64:5]'	
 						}
-					}
-				]
+					},
+					'postcss-loader'
+				],
 			},
+			
 		],
 	},
 	resolve: {
