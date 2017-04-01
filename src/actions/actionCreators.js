a33a3651cbae67e672bddb945498d979b091ccf2
Ivin Jose <ivinjose@gmail.com>
2017-04-02 02:14:37 +0530

    Got first Async response from API

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -2,6 +2,7 @@ import {ADD_SERVICE_DETAILS, ADD_SERVICE_DETAILS_ASYNC} from './actionConstants'
 import fetch from 'isomorphic-fetch';
 
 export function addServiceDetails(data){
+    console.log(data);
     return { 
         type: ADD_SERVICE_DETAILS,
         data: data
@@ -9,24 +10,12 @@ export function addServiceDetails(data){
 }
 
 export function addServiceDetailsAsync(data){
-    // let apiData = fetch('http://localhost:4001/addservicedetails').then(function(response){
-    //     return response;
-    // });
-    // console.log('apiData',apiData);
-    // return { 
-    //     type: PUSH_SERVICE_DETAILS,
-    //     data: data
-    // };
-    
-    /*
-    * New attempt to learn dispatch thunking
-    */
     return function(dispatch){
-         setTimeout(function(){
-            dispatch({
-                type: ADD_SERVICE_DETAILS,
-                data
-            })
-        },2000);
+        fetch('http://localhost:4001/addservicedetails')
+            .then(function(response){
+                return response.text();              
+            }).then(function(response){
+                dispatch( addServiceDetails( response ) );
+            });
     }
 }
