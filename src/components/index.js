f65af7dfb0fb42e2a63a35f652a4d468836c3fb3
Ivin Jose <ivinjose@gmail.com>
2017-04-04 10:41:19 +0530

    Using Provider from react-redux

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 import serviceDetailsReducer from '../reducers/serviceDetailsReducer';
@@ -8,22 +9,6 @@ import Home from './Home';
 import styles from '../styles/global.css';
 
 const store = createStore(serviceDetailsReducer, [ 'Use Redux' ], applyMiddleware(ReduxThunk));
-
-class Provider extends React.Component{
-	getChildContext(){
-		return{
-			store: this.props.store
-		};
-	}
-
-	render(){
-		return this.props.children;
-	}
-}
-
-Provider.childContextTypes = {
-	store: React.PropTypes.object
-};
  
 document.addEventListener('DOMContentLoaded', function() {
 	ReactDOM.render(
