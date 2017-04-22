c0b10a60f9a19497922265a2d925526cd37641c6
Ivin Jose <ivinjose@gmail.com>
2017-04-22 14:41:50 +0530

    react-router setup

--- server.js
+++ server.js
@@ -3,23 +3,25 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const app = express();
- 
+const path = require('path');
 const compiler = webpack(webpackConfig);
- 
-app.use(express.static(__dirname + '/www'));
- 
+
 app.use(webpackDevMiddleware(compiler, {
-  hot: true,
-  filename: 'bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
+	hot: true,
+	filename: 'bundle.js',
+	publicPath: '/',
+	stats: {
+		colors: true,
+	},
+	historyApiFallback: true,
 }));
  
+app.get('/*', (req,res) => {
+	res.sendFile(path.join(__dirname, '/www/index.html'))
+});
+
 const server = app.listen(4000, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
--- src/components/index.js
+++ src/components/index.js
@@ -4,18 +4,26 @@ import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk'
 import ReduxFreeze from 'redux-freeze'
 import { createStore, applyMiddleware } from 'redux'
+import { Router, Route, browserHistory } from 'react-router';
 import serviceDetailsReducer from '../reducers/serviceDetailsReducer';
-
-import Home from './Home';
 import styles from '../styles/global.css';
+import Home from './Home';
+import ViewServiceDetails from './ViewServiceDetails';
 
 const store = createStore( serviceDetailsReducer, { loading: true, data: [] }, applyMiddleware( ReduxThunk, ReduxFreeze ) );
+
+const Root = () => (
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/view" component={ViewServiceDetails} />
+			<Route path="/" component={Home} />
+		</Router>
+	</Provider>
+);
  
 document.addEventListener('DOMContentLoaded', function() {
 	ReactDOM.render(
-		<Provider store={store}>
-			<Home/>
-		</Provider>,
+		<Root />,
 		document.getElementById('mount')
 	);
 });
