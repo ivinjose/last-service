57d4ab16484c25fff13f66e31a06f8182b41c54b
Cijo KB <cijo.kb@gmail.com>
2017-09-14 22:10:33 +0530

    Adding basic style for view details page (#7)
    * Adding basic style for view details page , Adding inlinin soruce maping in webpack #6
    
    * PR-review: fixing pt->px

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -3,34 +3,57 @@ import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import styles from './ServiceDetails.css';
 import globalStyles from '../../styles/global.css';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 
 class ServiceDetails extends React.Component {
 	constructor() {
 		super();
 	}
- 
+
+  formatDate(date){
+	   return new Date(date).toLocaleDateString(); // Todo
+    };
 	render() {
-		if( this.props.data.length == 0 ){
+        const me =this;
+	    const data = this.props.data;
+		if( data.length === 0 ){
 			return(
 				<div>No data available!</div>
 			)
 		}
 		return (
-			<table className={styles['service-details']}>
-				{this.props.data.map(function(serviceDetail, index){
-					return (
-						<tr className={classNames(globalStyles['row'],styles['row'])} key={index}>
-							<td className={styles['cell']}>{serviceDetail.vehicle}</td>
-							<td className={styles['cell']}>{serviceDetail.itemName}</td>
-							<td className={styles['cell']}>{serviceDetail.itemService}</td>
-							<td className={styles['cell']}>{serviceDetail.date}</td>
-							<td className={styles['cell']}>{serviceDetail.kmsReading} kms</td>
-							<td className={styles['cell']}>{serviceDetail.amount} INR</td>
-						</tr>
-					)
-				})}			
-			</table>
-		);
+
+		    <div ref="serviceItem" className="service-details">
+                {data.map(function (serviceDetail, index) {
+                    return (<Card key={index} style ={{marginBottom:'5px'}}>
+                        <CardHeader
+                            title={serviceDetail.itemService}
+                            subtitle={me.formatDate(serviceDetail.date)}
+                            actAsExpander={true}
+                            showExpandableButton={true}
+                        />
+                        <CardText expandable={true}>
+                                <table>
+                                    <tbody>
+                                    <tr>
+                                        <td>Amount</td>
+                                        <td>{serviceDetail.amount}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{serviceDetail.vehicle}</td>
+                                        <td>{serviceDetail.itemName}</td>
+                                        <td>{serviceDetail.itemService}</td>
+                                        <td>{serviceDetail.kmsReading} kms</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                        </CardText>
+                    </Card>)
+                })
+                }
+                </div>
+        );
+
 	}
 }
 
