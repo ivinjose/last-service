d865d938caefa83d73e84ad6bd8457bfe1afcdca
Ivin Jose <ivinjose@gmail.com>
2018-02-08 18:10:50 +0530

    removing direct api calls from viewVehicleDetails and AddServiceDetails

--- src/components/ViewVehicleDetails/functions.js
+++ src/components/ViewVehicleDetails/functions.js
@@ -5,23 +5,4 @@ export default{
     addNew(){
 		browserHistory.push( routes[1].path );
 	},
-
-	getVehiclesList(){
-		return fetch('http://localhost:4001/getvehicles', 
-		{ 
-			method: 'GET', 
-			headers: {
-				'Content-Type': 'application/json'
-			}
-				   
-		}).then(function(response){
-			return( response.text() );
-		}).then(function(response){
-			return JSON.parse(response);
-		}).then(function(response){
-			return response;
-		}).catch(function(error){
-			return error;
-		});
-	}
 }
