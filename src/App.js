1924ec1bcd38397a3cc66edba9447319b6a1716e
Ivin Jose <ivinjose@gmail.com>
2018-02-22 13:18:58 +0530

    prettier on save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-import * as actionCreators from './actions/index';
-import Main from './components/Main';
+import * as actionCreators from "./actions/index";
+import Main from "./components/Main";
 
-function mapStateToProps( state ){
-    return{
+function mapStateToProps(state) {
+    return {
         user: state.user,
         vehicles: state.vehicles,
         services: state.services,
@@ -14,14 +14,14 @@ function mapStateToProps( state ){
     };
 }
 
-function mapDispatchToProps( dispatch ){
-    return bindActionCreators( actionCreators, dispatch );
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
 }
 
 /**
  * Wrapping connect with withRouter to let the shouldComponentUpdate know abut the location change
  * Ref: https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/blocked-updates.md
  */
-const App = withRouter( connect( mapStateToProps, mapDispatchToProps )(Main) );
+const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
 export default App;
--- src/components/AddServiceDetails/AddServiceDetails.js
+++ src/components/AddServiceDetails/AddServiceDetails.js
@@ -1,163 +1,180 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import Header from '../common/Header';
-import SubHeader from '../common/SubHeader';
-import ServicedItem from './AddServicedItem';
-import styles from './AddServiceDetails.css';
-import globalStyles from '../../styles/global.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Header from "../common/Header";
+import SubHeader from "../common/SubHeader";
+import ServicedItem from "./AddServicedItem";
+import styles from "./AddServiceDetails.css";
+import globalStyles from "../../styles/global.css";
 
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
-import DatePicker from 'material-ui/DatePicker';
-import AutoComplete from 'material-ui/AutoComplete';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import Snackbar from 'material-ui/Snackbar';
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import DatePicker from "material-ui/DatePicker";
+import AutoComplete from "material-ui/AutoComplete";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+import Snackbar from "material-ui/Snackbar";
 
 let DateTimeFormat = global.Intl.DateTimeFormat; //IntlPolyfill.DateTimeFormat;
 
 const serviceableParts = [
-	'Engine oil',
-	'Break fluid',
-	'Air filter',
-	'Break disc',
-	'Front wiper blade',
-	'Read wiper blade',
+    "Engine oil",
+    "Break fluid",
+    "Air filter",
+    "Break disc",
+    "Front wiper blade",
+    "Read wiper blade"
 ];
 
 class AddServiceDetails extends React.Component {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		this.state = {
-			currentVehicle: null,
-			snackbarState: false,
-			snackbarMessage: " ",
-		};
-	}
+        this.state = {
+            currentVehicle: null,
+            snackbarState: false,
+            snackbarMessage: " "
+        };
+    }
 
-	render() {
-		return (
-			<div className={styles['service-details']}>
-				<SubHeader text={"ADD SERVICE DETAILS"} />
-				<div className={styles['body']}>
-					<div className={globalStyles['row']}>
-						<SelectField hintText="Choose your vehicle" fullWidth={true} value={this.state.currentVehicle} onChange={this.updateVehicle.bind(this)}>
-						{
-							this.props.vehicles.map(function(vehicle, index){
-								return(
-									<MenuItem key={vehicle._id} value={vehicle.name} primaryText={vehicle.name} />
-								)
-							})
-						}
-						</SelectField>
-					</div>
+    render() {
+        return (
+            <div className={styles["service-details"]}>
+                <SubHeader text={"ADD SERVICE DETAILS"} />
+                <div className={styles["body"]}>
+                    <div className={globalStyles["row"]}>
+                        <SelectField
+                            hintText="Choose your vehicle"
+                            fullWidth={true}
+                            value={this.state.currentVehicle}
+                            onChange={this.updateVehicle.bind(this)}
+                        >
+                            {this.props.vehicles.map(function(vehicle, index) {
+                                return <MenuItem key={vehicle._id} value={vehicle.name} primaryText={vehicle.name} />;
+                            })}
+                        </SelectField>
+                    </div>
 
-					<div className={globalStyles['row']}>
-						<DatePicker 
-							hintText="On which date service happened?" 
-							onChange={this.updateDate.bind(this)}
-							fullWidth={true}
-							formatDate={new DateTimeFormat('en-US', {
-								day: 'numeric',
-								month: 'long',
-								year: 'numeric',
-							}).format}/>
-					</div>
+                    <div className={globalStyles["row"]}>
+                        <DatePicker
+                            hintText="On which date service happened?"
+                            onChange={this.updateDate.bind(this)}
+                            fullWidth={true}
+                            formatDate={
+                                new DateTimeFormat("en-US", {
+                                    day: "numeric",
+                                    month: "long",
+                                    year: "numeric"
+                                }).format
+                            }
+                        />
+                    </div>
 
-					<div  className={globalStyles['row']}>
-						<AutoComplete 
-							hintText="Which part was serviced?" 
-							dataSource={serviceableParts} 
-							fullWidth={true}
-							filter={AutoComplete.caseInsensitiveFilter} 
-							onUpdateInput={this.updateServicedComponent.bind(this)}
-							/>
-					</div>
+                    <div className={globalStyles["row"]}>
+                        <AutoComplete
+                            hintText="Which part was serviced?"
+                            dataSource={serviceableParts}
+                            fullWidth={true}
+                            filter={AutoComplete.caseInsensitiveFilter}
+                            onUpdateInput={this.updateServicedComponent.bind(this)}
+                        />
+                    </div>
 
-					<div  className={globalStyles['row']}>
-						<TextField hintText="How much you paid?" fullWidth={true} onChange={this.updateAmount.bind(this)} />
-					</div>
+                    <div className={globalStyles["row"]}>
+                        <TextField
+                            hintText="How much you paid?"
+                            fullWidth={true}
+                            onChange={this.updateAmount.bind(this)}
+                        />
+                    </div>
 
-					<div  className={globalStyles['row']}>
-						<TextField hintText="Any comments? (Optional)" fullWidth={true} onChange={this.updateComments.bind(this)} />
-					</div>
+                    <div className={globalStyles["row"]}>
+                        <TextField
+                            hintText="Any comments? (Optional)"
+                            fullWidth={true}
+                            onChange={this.updateComments.bind(this)}
+                        />
+                    </div>
 
-					<div  className={globalStyles['row']}>
-						<RaisedButton label="Save" primary={true} fullWidth={true} onClick={this.saveServicedItem.bind(this)}/>
-					</div>
-				</div>
+                    <div className={globalStyles["row"]}>
+                        <RaisedButton
+                            label="Save"
+                            primary={true}
+                            fullWidth={true}
+                            onClick={this.saveServicedItem.bind(this)}
+                        />
+                    </div>
+                </div>
 
-				<Snackbar
-					open={this.state.snackbarState}
-					message={this.state.snackbarMessage}
-					autoHideDuration={2000}
-					onRequestClose={this.handleRequestClose} />
+                <Snackbar
+                    open={this.state.snackbarState}
+                    message={this.state.snackbarMessage}
+                    autoHideDuration={2000}
+                    onRequestClose={this.handleRequestClose}
+                />
+            </div>
+        );
+    }
 
-			</div>
-		);
-	}
+    closeSnackbar() {
+        this.setState({
+            snackbarState: false
+        });
+    }
 
-	closeSnackbar(){
-		this.setState({
-			snackbarState: false
-		});
-	}
+    updateVehicle(event, key, payload) {
+        this.setState({
+            currentVehicle: payload
+        });
+    }
 
-	updateVehicle(event, key, payload){
-		this.setState({
-			currentVehicle: payload
-		});
-	}
+    updateDate(event, date) {
+        this.setState({
+            date: date
+        });
+    }
 
-	updateDate(event, date){
-		this.setState({
-			date: date
-		});
-	}
+    updateServicedComponent(searchText, dataSource, params) {
+        this.setState({
+            component: searchText
+        });
+    }
 
-	updateServicedComponent(searchText, dataSource, params){
-		this.setState({
-			component: searchText
-		});
-	}
+    updateAmount(event, newValue) {
+        this.setState({
+            amount: newValue
+        });
+    }
 
-	updateAmount(event, newValue){
-		this.setState({
-			amount: newValue
-		});
-	}
+    updateComments(event, newValue) {
+        this.setState({
+            comments: newValue
+        });
+    }
 
-	updateComments(event, newValue){
-		this.setState({
-			comments: newValue
-		});
-	}
+    saveServicedItem(e) {
+        let { store } = this.context;
+        let currentVehicle = { vehicle: this.state.currentVehicle };
+        let date = this.state.date;
+        let component = this.state.component;
+        let amount = this.state.amount;
+        let comments = this.state.comments;
 
-	saveServicedItem(e){
-		let { store } = this.context;
-		let currentVehicle = { vehicle: this.state.currentVehicle };
-		let date = this.state.date;
-		let component = this.state.component;
-		let amount = this.state.amount;
-		let comments = this.state.comments;
+        let service = {
+            vehicle: this.state.currentVehicle,
+            date,
+            component,
+            amount,
+            comments
+        };
 
-		let service = { 
-			vehicle: this.state.currentVehicle,
-			date,
-			component,
-			amount,
-			comments,
-		};
-
-		this.props.addServices( [service] );
-	}
+        this.props.addServices([service]);
+    }
 }
 
 AddServiceDetails.contextTypes = {
-	store: PropTypes.object
+    store: PropTypes.object
 };
 
 export default AddServiceDetails;
