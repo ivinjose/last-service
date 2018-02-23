4e47c80f88e7b1b8f6b24ecc43f4b93f07742406
Ivin Jose <ivinjose@gmail.com>
2018-02-23 19:37:56 +0530

    prettifying

--- src/actions/getVehicleActions.js
+++ src/actions/getVehicleActions.js
@@ -1,43 +1,46 @@
-import fetch from 'isomorphic-fetch';
+import fetch from "isomorphic-fetch";
 
-export function getAllVehicles( ){
+export function getAllVehicles() {
     return {
-        type: 'GET_ALL_VEHICLES_REQUEST',
+        type: "GET_ALL_VEHICLES_REQUEST",
         data: null
     };
 }
 
-export function getAllVehiclesAsync( ){
-    return function (dispatch){
-        dispatch( getAllVehicles() );
-        fetch('http://localhost:4001/vehicles',
-            { 
-                method: 'GET',
-                headers: {
-				    'Content-Type': 'application/json'
-			    }         
-            }).then(function(response){
-                return( response.text() );
-            }).then(function(response){
+export function getAllVehiclesAsync() {
+    return function(dispatch) {
+        dispatch(getAllVehicles());
+        fetch("http://localhost:4001/vehicles", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(function(response) {
+                return response.text();
+            })
+            .then(function(response) {
                 return JSON.parse(response);
-            }).then(function(response){
-                dispatch( getAllVehiclesSuccess( response ) );
-            }).catch(function(error){
-                dispatch( getAllVehiclesFailure( error ) );
+            })
+            .then(function(response) {
+                dispatch(getAllVehiclesSuccess(response));
+            })
+            .catch(function(error) {
+                dispatch(getAllVehiclesFailure(error));
             });
-    }
+    };
 }
 
-export function getAllVehiclesSuccess( response ){
+export function getAllVehiclesSuccess(response) {
     return {
-        type: 'GET_ALL_VEHICLES_SUCCESS',
+        type: "GET_ALL_VEHICLES_SUCCESS",
         vehicles: response.data
     };
 }
 
-export function getAllVehiclesFailure( error ){
+export function getAllVehiclesFailure(error) {
     return {
-        type: 'GET_ALL_VEHICLES_FAILURE',
+        type: "GET_ALL_VEHICLES_FAILURE",
         data: { ...error }
     };
 }
