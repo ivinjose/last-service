457d51f43711cc3da5e8873773d8cc3c07d23dfa
Ivin Jose <ivinjose@gmail.com>
2018-02-26 20:57:18 +0530

    Cleaning up services page

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styles from './ServiceDetails.css';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
-import svg from '../../images/notfound.svg';
-import ServiceDetailsCard from './ServiceDetailCard';
+import React from "react";
+import styles from "./ServiceDetails.css";
+import { Card, CardHeader, CardText } from "material-ui/Card";
+import svg from "../../images/notfound.svg";
+import ServiceDetailsCard from "./ServiceDetailCard";
 
 class ServiceDetails extends React.Component {
     constructor(props) {
@@ -10,37 +10,34 @@ class ServiceDetails extends React.Component {
         this.formatDateTime = this.formatDateTime.bind(this);
     }
 
-    formatDateTime (date) {
+    formatDateTime(date) {
         const newDate = new Date(date);
         return {
             date: newDate.toLocaleDateString(),
             time: newDate.toLocaleTimeString()
         };
-    };
+    }
 
     render() {
         if (this.props.data.length === 0) {
             return (
-                <div className={styles['service-details']}>
-                    <div className={styles['NoDataFound']}>
-                        <div className={styles['Header']}>
-                            <h3>"Oops! no data available"</h3>
+                <div className={styles["service-details"]}>
+                    <div className={styles["no-data"]}>
+                        <div className={styles["Header"]}>
+                            <h4>Looks like you have not added any services yet.</h4>
+                            <h4>Why don't you add some?</h4>
                         </div>
-                        <div className={styles['Avatar']}>
-                            <img src={`/images/${svg}`}/>
-                        </div>
+                        <div className={styles["avatar"]}>{/* <img src={`/images/${svg}`} /> */}</div>
                     </div>
                 </div>
             );
         }
 
         return (
-		    <div ref="serviceItem" className={styles['service-details']}>
-                {
-                    this.props.data.map( (service, index)=>{
-                        return <ServiceDetailsCard key={index} service={service} />
-                    })
-                }
+            <div ref="serviceItem" className={styles["service-details"]}>
+                {this.props.data.map((service, index) => {
+                    return <ServiceDetailsCard key={index} service={service} />;
+                })}
             </div>
         );
     }
