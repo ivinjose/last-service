e3d87cb885cdc6ee068ff151b30f9a2d141eba30
Ivin Jose <ivinjose@gmail.com>
2018-02-27 22:23:46 +0530

    SnackbarMessage support on addServices flow

--- src/actions/addServicesActions.js
+++ src/actions/addServicesActions.js
@@ -1,45 +1,48 @@
-import fetch from 'isomorphic-fetch';
+import fetch from "isomorphic-fetch";
 
-export function addServicesInit( details ){
+export function addServicesInit(details) {
     return {
-        type: 'ADD_SERVICES_INIT',
+        type: "ADD_SERVICES_INIT",
         data: { ...details }
     };
 }
 
-export function addServices( services ){
+export function addServices(services) {
     return (dispatch) => {
-        dispatch( addServicesInit() );
-        fetch('http://localhost:4001/services', 
-            { 
-                method: 'POST', 
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify( services ) 
-                       
-            }).then(function(response){
-                return( response.text() );
-            }).then(function(response){
+        dispatch(addServicesInit());
+        fetch("http://localhost:4001/services", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(services)
+        })
+            .then(function(response) {
+                return response.text();
+            })
+            .then(function(response) {
                 return JSON.parse(response);
-            }).then(function(response){
-                dispatch( addServicesSuccess( response.data ) );
-            }).catch(function(error){
-                dispatch( addServicesFailure( error ) );
+            })
+            .then(function(response) {
+                dispatch(addServicesSuccess(response));
+            })
+            .catch(function(error) {
+                dispatch(addServicesFailure(error));
             });
-    }
-}
-
-export function addServicesSuccess( servicesAdded ){
-    return {
-        type: 'ADD_SERVICES_SUCCESS',
-        servicesAdded
     };
 }
 
-export function addServicesFailure( error ){
+export function addServicesSuccess({ servicesAdded, message }) {
     return {
-        type: 'ADD_SERVICES_FAILURE',
+        type: "ADD_SERVICES_SUCCESS",
+        servicesAdded,
+        message
+    };
+}
+
+export function addServicesFailure(error) {
+    return {
+        type: "ADD_SERVICES_FAILURE",
         data: { ...error }
     };
 }
--- src/reducers/uiReducer.js
+++ src/reducers/uiReducer.js
@@ -2,7 +2,7 @@ const ui = function(state = [], action) {
     let newState;
     switch (action.type) {
         /**
-         * GET USER VEHICLES, SERVICES actions
+         * GET USER VEHICLES, GET USER SERVICES actions
          */
         case "GET_USER_VEHICLES_INIT":
         case "GET_USER_SERVICES_INIT":
@@ -18,7 +18,7 @@ const ui = function(state = [], action) {
             break;
 
         /**
-         * UPDATE VEHICLE actions
+         * VEHICLE actions
          */
         case "ADD_VEHICLES_INIT":
         case "UPDATE_VEHICLE_INIT":
@@ -34,24 +34,34 @@ const ui = function(state = [], action) {
             break;
 
         /**
+         * SERVICE actions
+         */
+        case "ADD_SERVICES_INIT":
+            newState = Object.assign({}, state, { showPageBlockingLoader: true, snackbarMessage: null });
+            return newState;
+            break;
+        case "ADD_SERVICES_SUCCESS":
+        case "ADD_SERVICES_FAILURE":
+            newState = Object.assign({}, state, { showPageBlockingLoader: false, snackbarMessage: action.message });
+            return newState;
+            break;
+
+        /**
          * All other actions
          */
         case "GET_USER_INIT":
-        case "ADD_SERVICES_INIT":
         case "GET_ALL_VEHICLES_INIT":
         case "GET_ALL_SERVICES_INIT":
             newState = Object.assign({}, state, { showPageBlockingLoader: true, snackbarMessage: null });
             return newState;
             break;
         case "GET_USER_SUCCESS":
-        case "ADD_SERVICES_SUCCESS":
         case "GET_ALL_VEHICLES_SUCCESS":
         case "GET_ALL_SERVICES_SUCCESS":
             newState = Object.assign({}, state, { showPageBlockingLoader: false, snackbarMessage: null });
             return newState;
             break;
         case "GET_USER_FAILURE":
-        case "ADD_SERVICES_FAILURE":
         case "GET_ALL_VEHICLES_FAILURE":
         case "GET_ALL_SERVICES_FAILURE":
             newState = Object.assign({}, state, { showPageBlockingLoader: false, snackbarMessage: null });
