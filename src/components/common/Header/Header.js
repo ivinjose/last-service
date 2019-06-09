2ef9b313799be684fb7c244318dfe155f08e1d36
Ivin Jose <ivinjose@gmail.com>
2019-06-09 20:39:33 +0530

    Fixing drawer

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { browserHistory } from 'react-router'
 import styles from './Header.css'
+import clsx from 'clsx'
+
 import routes from '../../../routes/routes'
 
 import AppBar from '@material-ui/core/AppBar'
@@ -12,98 +14,149 @@ import Toolbar from '@material-ui/core/Toolbar'
 import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
 
-class Header extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            isDrawerOpen: false,
-        }
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+}))
+
+function Header(props) {
+    const classes = useStyles()
+    const theme = useTheme()
+    const [isDrawerOpen, openDrawer] = React.useState(false)
+
+    function handleDrawerOpen() {
+        openDrawer(true)
     }
 
-    render() {
-        return (
-            <div className={styles['header']}>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <IconButton
-                            color="inherit"
-                            aria-label="Open drawer"
-                            onClick={handleDrawerOpen}
-                            edge="start"
-                            className={clsx(
-                                classes.menuButton,
-                                open && classes.hide
-                            )}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" noWrap>
-                            Persistent drawer
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                   
-                <AppBar position="static">
-                    <IconButton edge="start" color="inherit" aria-label="Menu">
+    function handleDrawerClose() {
+        openDrawer(false)
+    }
+
+    return (
+        <div className={styles['header']}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="Open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(
+                            classes.menuButton,
+                            isDrawerOpen && classes.hide
+                        )}
+                    >
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6">Photos</Typography>
-                </AppBar>
-                <Drawer
-                    docked={false}
-                    width={250}
-                    open={this.state.isDrawerOpen}
-                    value={1}
-                    onRequestChange={isDrawerOpen =>
-                        this.setState({ isDrawerOpen })
-                    }
-                >
-                    <div className={styles['info-bar']}>
-                        <div className={styles['user-image-placeholder']}>
-                            <img
-                                src="https://d30y9cdsu7xlg0.cloudfront.net/png/17241-200.png"
-                                className={styles['image']}
-                                alt="User image"
-                            />
-                        </div>
-                        <div className={styles['username']}>Ivin Jose</div>
-                        <div className={styles['email']}>
-                            ivinjose@gmail.com
-                        </div>
-                    </div>
+                    <Typography variant="h6" noWrap>
+                        Persistent drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={isDrawerOpen}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? (
+                            <ChevronLeftIcon />
+                        ) : (
+                            <ChevronRightIcon />
+                        )}
+                    </IconButton>
+                </div>
+
+                <List>
                     {routes.map((route, index) => {
                         return (
-                            <MenuItem
-                                value={1}
-                                key={index}
-                                onClick={this.changeRoute.bind(this, index)}
-                            >
-                                {route.name}
-                            </MenuItem>
+                            <div>
+                                <ListItem button key={route.name} /*onClick={this.changeRoute.bind(this, index)}*/>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? (
+                                            <InboxIcon />
+                                        ) : (
+                                            <MailIcon />
+                                        )}
+                                    </ListItemIcon>
+                                    <ListItemText primary={route.name} />
+                                </ListItem>
+                            </div>
                         )
                     })}
-                </Drawer>
-            </div>
-        )
-    }
-
-    openDrawer() {
-        this.setState({
-            isDrawerOpen: true,
-        })
-    }
-
-    changeRoute(index) {
-        this.setState(
-            {
-                isDrawerOpen: false,
-            },
-            () => {
-                browserHistory.push(routes[index].path)
-            }
-        )
-    }
+                </List>
+            </Drawer>
+        </div>
+    )
 }
 
 Header.contextTypes = {
