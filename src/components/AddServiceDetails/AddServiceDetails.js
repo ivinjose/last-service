f8398c7946adada4a7e4bbdcacef2ea9712054e6
cijokb <cijo.kb@gmail.com>
2019-09-21 20:58:26 +0530

    feat(issue#43): clear the form details issue#43

--- src/components/AddServiceDetails/AddServiceDetails.js
+++ src/components/AddServiceDetails/AddServiceDetails.js
@@ -37,33 +37,38 @@ class AddServiceDetails extends React.Component {
 			currentVehicle: null,
 			snackbarState: false,
 			snackbarMessage: " ",
+			date: "",
+			amount: "",
+			comments: "",
 		};
+		this.closeSnackbar = this.closeSnackbar.bind(this);
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		this.getVehiclesList();
 	}
+
 
 	render() {
 		return (
 			<div className={styles['service-details']}>
-				<Header title={"Add service details"}/>
+				<Header title={"Add service details"} />
 
 				<div className={styles['body']}>
 					<div className={globalStyles['row']}>
 						<FormControl className={styles['form-control']}>
 							<InputLabel htmlFor="age-helper">Choose your vehicle</InputLabel>
-							<Select 
-								value={this.state.currentVehicle} 
+							<Select
+								value={this.state.currentVehicle}
 								onChange={this.updateVehicle.bind(this)}>
-							{
-								this.state.vehicles.map( vehicle => {
-									return(
-										<MenuItem key={vehicle._id} value={vehicle.name}>{vehicle.name}</MenuItem>
-									);
-								})
+								{
+									this.state.vehicles.map(vehicle => {
+										return (
+											<MenuItem key={vehicle._id} value={vehicle._id} >{vehicle.name}</MenuItem>
+										);
+									})
 
-							}
+								}
 							</Select>
 						</FormControl>
 					</div>
@@ -71,17 +76,13 @@ class AddServiceDetails extends React.Component {
 					<div className={globalStyles['row']}>
 						<TextField
 							type="date"
-							placeholder="On which date service happened?" 
+							placeholder="On which date service happened?"
 							onChange={this.updateDate.bind(this)}
 							fullWidth={true}
-							formatDate={new DateTimeFormat('en-US', {
-								day: 'numeric',
-								month: 'long',
-								year: 'numeric',
-							}).format}/>
+							value={this.state.date} />
 					</div>
 
-					<div  className={globalStyles['row']}>
+					<div className={globalStyles['row']}>
 						{/* <AutoComplete 
 							hintText="Which part was serviced?" 
 							dataSource={serviceableParts} 
@@ -92,11 +93,19 @@ class AddServiceDetails extends React.Component {
 					</div>
 
 					<div className={globalStyles['row']}>
-						<TextField placeholder="How much you paid?" fullWidth={true} onChange={this.updateAmount.bind(this)} />
+						<TextField
+							placeholder="How much you paid?"
+							fullWidth={true}
+							onChange={this.updateAmount.bind(this)}
+							value={this.state.amount} />
 					</div>
 
 					<div className={globalStyles['row']}>
-						<TextField placeholder="Any comments? (Optional)" fullWidth={true} onChange={this.updateComments.bind(this)} />
+						<TextField
+							placeholder="Any comments? (Optional)"
+							fullWidth={true}
+							onChange={this.updateComments.bind(this)}
+							value={this.state.comments} />
 					</div>
 
 					<div className={globalStyles['row']}>
@@ -108,110 +117,121 @@ class AddServiceDetails extends React.Component {
 					open={this.state.snackbarState}
 					message={this.state.snackbarMessage}
 					autoHideDuration={2000}
-					onRequestClose={this.handleRequestClose} />
+					onClose={this.closeSnackbar} />
 
 			</div>
 		);
 	}
 
-	getVehiclesList(){
+	getVehiclesList() {
 		var _this = this;
-		fetch('http://localhost:4001/users/5a86de0b90d792bccf3c3404/vehicles', 
-		{ 
-			method: 'GET', 
-			headers: {
-				'Content-Type': 'application/json'
-			}
-				   
-		}).then(function(response){
-			return( response.text() );
-		}).then(function(response){
-			return JSON.parse(response);
-		}).then(function(response){
-			_this.setState({
-				vehicles: response.data?response.data : []
+		fetch('http://localhost:4001/users/5a86de0b90d792bccf3c3404/vehicles',
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+
+			}).then(function (response) {
+				return (response.text());
+			}).then(function (response) {
+				return JSON.parse(response);
+			}).then(function (response) {
+				_this.setState({
+					vehicles: response.data ? response.data : []
+				});
+			}).catch(function (error) {
+				console.log('Error in AddServiceDetails', error);
 			});
-		}).catch(function(error){
-			console.log('Error in AddServiceDetails', error);
-		});
 	}
 
-	closeSnackbar(){
+	closeSnackbar() {
 		this.setState({
 			snackbarState: false
 		});
 	}
 
-	updateVehicle(event){
+	updateVehicle(event) {
 		this.setState({
 			currentVehicle: event.target.value
 		});
 	}
 
-	updateDate(event, date){
+	updateDate(event) {
 		this.setState({
-			date: date
+			date: event.target.value
 		});
 	}
 
-	updateServicedComponent(searchText, dataSource, params){
+	updateServicedComponent(searchText, dataSource, params) {
 		this.setState({
 			component: searchText
 		});
 	}
 
-	updateAmount(event, newValue){
+	updateAmount(event) {
 		this.setState({
-			amount: newValue
+			amount: event.target.value
 		});
 	}
 
-	updateComments(event, newValue){
+	updateComments(event) {
 		this.setState({
-			comments: newValue
+			comments: event.target.value
 		});
 	}
 
-	saveServicedItem(e){
-		let currentVehicle = { vehicle: this.state.currentVehicle };
-		let date = this.state.date;
-		let component = this.state.component;
-		let amount = this.state.amount;
-		let comments = this.state.comments;
 
-		let serviceDetails = { 
+	saveServicedItem(e) {
+		const id = this.state.currentVehicle;
+		const name = this.state.vehicles.find((vehicle) => vehicle._id === id).name;
+		const vehicleDetails = { vehicle: id, name };
+		const date = this.state.date;
+		//let component = this.state.component;
+		const amount = this.state.amount;
+		const comments = this.state.comments;
+
+		const serviceDetails = {
 			date: date,
-			component: component,
+			//component: component,
 			amount: amount,
 			comments: comments,
 		};
 
-		let data = Object.assign( {}, currentVehicle, serviceDetails );
-		let _this = this;
+		const data = { ...vehicleDetails, ...serviceDetails };
+		const _this = this;
 
-		fetch('http://localhost:4001/services', 
-		{ 
-			method: 'POST', 
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data) 
-				   
-		}).then(function(response){
-			return( response.text() );
-		}).then(function(response){
-			return JSON.parse(response);
-		}).then(function(response){
-			_this.setState({
-				snackbarState: true,
-				snackbarMessage: response.message
+		fetch(`http://localhost:4001/vehicles/${id}/service`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+
+			}).then(function (response) {
+				return (response.text());
+			}).then(function (response) {
+				return JSON.parse(response);
+			}).then(function (response) {
+				_this.setState({
+					snackbarState: true,
+					snackbarMessage: response.message,
+					currentVehicle: null,
+					date: "",
+					comments: "",
+					amount: "",
+				});
+			}).catch(function (error) {
+				_this.setState({
+					snackbarState: true,
+					snackbarMessage: error,
+					currentVehicle: null,
+					date: "",
+					comments: "",
+					amount: "",
+				});
 			});
-		}).catch(function(error){
-			_this.setState({
-				snackbarState: true,
-				snackbarMessage: error
-			});
-		});
 
 	}
 }
