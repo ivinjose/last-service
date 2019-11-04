c2f79482ab692189d2b6d2f6b58b66d56e25c43d
Ivin Jose <ivin.jose@flipkart.com>
2019-11-04 17:56:51 +0530

    Rearranging the store to make it look neater

--- src/state/store.js
+++ src/state/store.js
@@ -1,40 +1,46 @@
 import createStore from 'storeon';
 import makeApiCall from "../utils/ApiHelper";
 
-const user = store => {
-    store.on('@init', ()=>({ user: {isLoggedIn: false} }));
-    store.on('user/login', async (state, userCredentials)=>{
-        store.dispatch('loading:true');
-        const user = await makeApiCall("http://localhost:4001/login", { method: 'POST', body: userCredentials });
-        store.dispatch('user/login:success', user);
-    });
-    store.on('user/login:success', (state, data)=>{
-        store.dispatch('loading:false');
-        return { user: { isLoggedIn: true, ...data } };
-    });
-    store.on('user/login:error', ()=>{
-        store.dispatch('loading:false');
-        return{ user: {isLoggedIn: false} }
-    });
-}
-
 const loading = store => {
     store.on('@init', ()=>({ loading: false }));
     store.on('loading:true', ()=>({ loading: true }));
     store.on('loading:false', ()=>({ loading: false }));
 }
 
+const user = store => {
+    store.on('@init', ()=>({ user: {isLoggedIn: false} }));
+
+    store.on('user/login', async (state, userCredentials)=>{
+        store.dispatch('loading:true');
+        const user = await makeApiCall("http://localhost:4001/login", { method: 'POST', body: userCredentials });
+        store.dispatch('user/login:success', user);
+    });
+
+    store.on('user/login:success', (state, data)=>{
+        store.dispatch('loading:false');
+        return { user: { isLoggedIn: true, ...data } };
+    });
+    
+    store.on('user/login:error', ()=>{
+        store.dispatch('loading:false');
+        return{ user: {isLoggedIn: false} }
+    });
+}
+
 const vehicles = store => {
     store.on('@init', ()=>({ vehicles: [] }));
+
     store.on('vehicles/get', async (state, userId)=>{
         store.dispatch('loading:true');
         const vehicles = await makeApiCall("http://localhost:4001/users/" + userId + "/vehicles", { method: 'GET' });
         store.dispatch('vehicles/get:success', vehicles);
     });
+
     store.on('vehicles/get:success', (state, data)=>{
         store.dispatch('loading:false');
         return { vehicles: data };
     });
+
     store.on('vehicles/get:error', (state, error)=>{
         store.dispatch('loading:false');
         console.log('Error in fetching vehicles', error);
