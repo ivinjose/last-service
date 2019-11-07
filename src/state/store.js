74e58f3773a28c39e66ccd6c9523f2bd0f1405fb
Ivin Jose <ivin.jose@flipkart.com>
2019-11-07 11:19:59 +0530

    Moving add vehicles page to store logic

--- src/state/store.js
+++ src/state/store.js
@@ -46,6 +46,23 @@ const vehicles = store => {
         console.log('Error in fetching vehicles', error);
         return { vehicles: [] };
     });
+
+    store.on('vehicles/add', async (state, data)=>{
+        store.dispatch('loading:true');
+        const newVehicles = await makeApiCall("http://localhost:4001/users/" + data.userId + "/vehicles", { method: 'POST', body: data.vehicles });
+        store.dispatch('vehicles/add:success', newVehicles);
+    });
+
+    store.on('vehicles/add:success', (state, newVehicles)=>{
+        store.dispatch('loading:false');
+        return { vehicles: [ ...state.vehicles, ...newVehicles ] };
+    });
+
+    store.on('vehicles/add:error', (state, error)=>{
+        store.dispatch('loading:false');
+        console.log('Error in adding vehicles', error);
+        return { vehicles: state.vehicles };
+    });
 }
 
 const services = store => {
