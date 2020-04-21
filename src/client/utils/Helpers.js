758db6de3bac8f59da48309540fa08a1917680cd
Ivin Jose <ivin.jose@flipkart.com>
2020-04-21 17:20:21 +0530

    removing unused prettifyDate

--- src/client/utils/Helpers.js
+++ src/client/utils/Helpers.js
@@ -1,18 +1,6 @@
 
 import moment from 'moment';
 
-export const prettifyDate = date =>{
-	if( !date || date === " " ){
-		return 'Invalid date';
-	}
-	const d = new Date(date)
-	const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
-	const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d)
-	const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
-	
-	return `${mo} ${da}, ${ye}`;
-};
-
 /**
  * Note for moment:
  * The onChange event gives a moment object
