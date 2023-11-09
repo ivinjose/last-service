45986cc7de3b3a443957cceb8ba2b4be137e39fb
Ivin Jose <ivinjose@Ivins-MacBook-Pro.local>
2023-11-09 08:12:48 +0530

    test

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { 
+import {
 	BrowserRouter as Router,
 	Route,
 } from 'react-router-dom';
@@ -8,27 +8,27 @@ import { routes } from "../routes/routes";
 import Loader from './common/Loader';
 import Snackbar from '@material-ui/core/Snackbar';
 import SecureRoute from './common/SecureRoute';
-import globalStyles from '../styles/global.css';
+// import globalStyles from '../styles/global.css';
 
-const App = () => { 
+const App = () => {
 	const { loading, user, snackbarMessage, dispatch } = useStoreon('loading', 'user', 'snackbarMessage');
 	return (
 		<React.Fragment>
 			<Loader loading={loading} />
 			<Router>
 				{
-					routes.map((routeObj)=>{
+					routes.map((routeObj) => {
 						return routeObj.isSecure
-						? <SecureRoute exact user={user} {...routeObj} />
-						: <Route exact {...routeObj} />;
+							? <SecureRoute exact user={user} {...routeObj} />
+							: <Route exact {...routeObj} />;
 					})
 				}
 			</Router>
 			<Snackbar
-					open={snackbarMessage.show}
-					message={snackbarMessage.message}
-					autoHideDuration={2500}
-					onClose={()=>dispatch('snackbar:hide')} />
+				open={snackbarMessage.show}
+				message={snackbarMessage.message}
+				autoHideDuration={2500}
+				onClose={() => dispatch('snackbar:hide')} />
 		</React.Fragment>
 	)
 };
